310b841c7de597691f06b4194416b6068b360921
Imwy345 <kostay2013.18@gmail.com>
2025-03-03 02:22:06 +0300

    Category:Service,Entity,Repository,Controller

--- src/main/java/com/jmp/template/exception/DuplicateCategoryException.java
+++
@@ -1,8 +0,0 @@
-package com.jmp.template.exception;
-
-public class DuplicateCategoryException extends RuntimeException {
-    public DuplicateCategoryException(String message) {
-        super(message);
-    }
-}
-
--- src/main/java/com/jmp/template/model/Category.java
+++ src/main/java/com/jmp/template/model/Category.java
@@ -4,28 +4,21 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "category")
